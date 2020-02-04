77a9d46a31d592ac7cffcd5842afd89593246f65
caiqueribeiro <caique.rodrigues2@fatec.sp.gov.br>
2020-02-03 22:12:12 -0300

    Listagem de prestadores e função de agendamento

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './configs/multer';
@@ -14,13 +15,15 @@ const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
-
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.get('/appointments', AppointmentController.index);
+
+routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
